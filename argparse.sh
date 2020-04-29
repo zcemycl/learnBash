ed896bd6fa2fba76fecc4031a41124593cb07afe
zcemycl <yui.leung.15@ucl.ac.uk>
2020-04-30 03:21:34 +0800

    add example of option command

---
+++ argparse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while getopts u:d:p:f: option
+do
+	case "${option}"
+	in
+		u) USER=${OPTARG};;
+		d) DATE=${OPTARG};;
+		p) PRODUCT=${OPTARG};;
+		f) FORMAT=${OPTARG};;
+	esac
+done
+
+echo $USER
+echo $DATE
+echo $PRODUCT
+echo $FORMAT
